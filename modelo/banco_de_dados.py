39cd50c07e55e3808fcee3ba1a41af45eab85f07
nw-111 <joaovictorguilhermeribeiro14@gmail.com>
2026-04-12 01:00:07 -0300

    documentando o banco de dados feito com lista

--- modelo/banco_de_dados.py
+++ modelo/banco_de_dados.py
@@ -1,44 +1,45 @@
 class Bancodedados:
-    _hospedes_ = []             #"o banco de dados vao ser listas por enquanto, so vai salvar na memoria ram mesmo"
+    _hospedes_ = []             #as variaveis vao ficar aqui mesmo, nao vamos declarar o banco de dados com um objeto, vamos apenas usar a classe.
     _quartos_ = []
-    _reservas_ = []
+    _reservas_ = []             #o banco de dados vao ser listas por enquanto, so vai salvar na memoria ram mesmo"
 
     proximoidhospede = 1
-    proximoidquarto = 1
+    proximoidquarto = 1                          #IDs pra salvar os objetos nas listas, com um ID
     proximoidreserva = 1
     
     @classmethod
     def atualiza_hospede(cls, hospede):
-        ########
-        return
+        ########                                     #vazio, porque como estamos lidando com listas, tudo vai atualizar na memoria ram                             
+        return                                      #mas vou deixar aq pra quando formos usar sqlite, colocarmos a logica aqui.
 
     @classmethod
-    def salva_hospede(cls, hospede):
+    def salva_hospede(cls, hospede):             #OBS: o cls, serve pra alterar variaveis ou usar metodos da classe, independendo do objeto
+                                                            #assim, nosso banco de dados é uma coisa só
         if(hospede.id == None):
-            hospede.id = cls.proximoidhospede
-            cls.proximoidhospede += 1
+            hospede.id = cls.proximoidhospede           #salva o hospede na lista de hospedes com um id
+            cls.proximoidhospede += 1               
             cls._hospedes_.append(hospede)
         else:
-            cls.atualiza_hospede(hospede)
+            cls.atualiza_hospede(hospede)       #se o ID ja existir, ele vai atualizar.
     
     @classmethod
     def apaga_hospede(cls, hospede):
         if(hospede in cls._hospedes_):
-            cls._hospedes_.remove(hospede)
+            cls._hospedes_.remove(hospede)      #apaga o hospede da lista
         else:
-            #########
+            #########               #se o hospede nao existir a gente deve mostrar alguma mensagem talvez
             return
         
     @classmethod
-    def atualiza_quarto(cls, quarto):
-        #####
+    def atualiza_quarto(cls, quarto):      
+        #####                                   
         return
     
     @classmethod
 
     def salva_quarto(cls, quarto):
         if(quarto.id == None):
-            quarto.id = cls.proximoidquarto
+            quarto.id = cls.proximoidquarto     
             cls.proximoidquarto += 1
             cls._quartos_.append(quarto)
         else:
